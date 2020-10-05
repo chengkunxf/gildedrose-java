e01678c25bf17af9f91b02f5b758cf5f2b889610
chengkunxf <chengkunxf@126.com>
2020-10-05 08:14:39 +0800

    refactor:add safetyNetTest

---
+++ src/test/java/com/gildedrose/GildedRoseSafetyNetTest.java
@@ -0,0 +1,21 @@
+package com.gildedrose;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertThat;
+
+
+public class GildedRoseSafetyNetTest {
+
+    @Test
+    public void should_output_string_equals_base_line_string() throws IOException {
+        String baseline = Files.toString(new File("src/test/baseline.txt"), Charsets.UTF_8);
+        assertThat(TexttestFixture.getOutputString(), Is.is(baseline));
+    }
+}
