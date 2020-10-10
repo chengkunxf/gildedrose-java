6723e549c5dfc812148b24775df4e3a7baa59dbe
chengkunxf <chengkunxf@126.com>
2020-10-11 07:40:03 +0800

    refactor:子类覆盖 is 行为

--- src/main/java/com/gildedrose/item/Sulfuras.java
+++ src/main/java/com/gildedrose/item/Sulfuras.java
@@ -6,4 +6,9 @@ public class Sulfuras extends Item {
     public Sulfuras(int sellIn, int quality) {
         super(SULFURAS_HAND_OF_RAGNAROS, sellIn, quality);
     }
+
+    @Override
+    protected boolean isSulfuras() {
+        return true;
+    }
 }
