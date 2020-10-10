5282dc9c03e155642e5e8e6dabe054bb9149cec6
chengkunxf <chengkunxf@126.com>
2020-10-10 15:04:24 +0800

    refactor:增加测试安全网，可以覆盖 90%的业务，且可以一键运行，看到结果报告

---
+++ src/test/java/com/gildedrose/GildedRoseSafetyNetTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertThat;
+
+public class GildedRoseSafetyNetTest {
+
+    @Test
+    public void should_get_baseline_equals_get_out_put_string() throws IOException {
+        String baseline = Files.toString(new File("src/test/baseline.txt"), Charsets.UTF_8);
+        assertThat(TexttestFixture.getOutput(), Is.is(baseline));
+    }
+}
