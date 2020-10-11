a8a45cb735b09bc2acabfe524ab8ef9e4f659071
chengkunxf <chengkunxf@126.com>
2020-10-11 16:56:34 +0800

    refactor:逻辑下推到子类

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -51,32 +51,9 @@ public class Item {
             }
             return;
         }
-
-        if (isAgedBrie()
-                || isBackstagePass()) {
-            if (quality < 50) {
-                quality = quality + 1;
-
-                if (isBackstagePass()) {
-                    if (sellIn < 11) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-
-                    if (sellIn < 6) {
-                        if (quality < 50) {
-                            quality = quality + 1;
-                        }
-                    }
-                }
-            }
-            return;
-        }
         if (quality > 0) {
             quality = quality - 1;
         }
-
     }
 
     protected void updateQualityAfterExpiration() {
