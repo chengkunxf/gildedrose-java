f47ca42a32f8d6ee907cd4ed261df45b0de0a304
chengkunxf <chengkunxf@126.com>
2020-10-11 16:48:12 +0800

    refactor:override updateQualityAfterExpiration sulfuras

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -86,13 +86,8 @@ public class Item {
             return;
         }
         if (quality > 0) {
-            if (isSulfuras()) {
-                return;
-            }
             quality = quality - 1;
         }
-
-
     }
 
     protected boolean isSulfuras() {
--- src/main/java/com/gildedrose/item/Sulfuras.java
+++ src/main/java/com/gildedrose/item/Sulfuras.java
@@ -16,4 +16,9 @@ public class Sulfuras extends Item {
     protected void updateSellIn() {
 
     }
+
+    @Override
+    protected void updateQualityAfterExpiration() {
+
+    }
 }
