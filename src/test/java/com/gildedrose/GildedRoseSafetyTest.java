b0647c1aa467413aba83e291b4345bfd3cf3af8e
chengkunxf <chengkunxf@126.com>
2020-10-22 18:12:29 +0800

    refactor:增加测试安全网

---
+++ src/test/java/com/gildedrose/GildedRoseSafetyTest.java
@@ -0,0 +1,20 @@
+package com.gildedrose;
+
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+import org.hamcrest.core.Is;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertThat;
+
+public class GildedRoseSafetyTest {
+
+    @Test
+    public void should_get_out_put_string_equals_base_line() throws IOException {
+        String baseline = Files.toString(new File("src/test/baseline.txt"), Charsets.UTF_8);
+        assertThat(TexttestFixture.getOutput(), Is.is(baseline));
+    }
+}
